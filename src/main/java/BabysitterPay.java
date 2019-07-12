7338ddac21944e4cf08cffa07a6a1a996228db94
Nicholas Land <land.d.nicholas@gmail.com>
2019-07-12 12:46:37 -0400

    Add test for end time being earlier than start time

--- src/main/java/BabysitterPay.java
+++ src/main/java/BabysitterPay.java
@@ -1,33 +1,36 @@
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public class BabysitterPay {
 
     private String family;
-    private LocalTime startTime;
-    private LocalTime endTime;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private double pay;
 
     public BabysitterPay(String family) {
         this.family = family;
     }
 
-    public void startTime(LocalTime startTime) throws Exception {
-        LocalTime fiveOClockPM = LocalTime.parse("17:00");
-        if (fiveOClockPM.compareTo(startTime) > 0) {
+    public void startTime(LocalDateTime startTime) throws Exception {
+        int fiveOClockPM = 17;
+        if (fiveOClockPM > startTime.getHour() && startTime.getHour() > 4) {
             throw new Exception("Outside of working hours.");
         }
         this.startTime = startTime;
     }
 
-    public void endTime(LocalTime endTime) throws Exception {
-        LocalTime fourOClockAM = LocalTime.parse("04:00");
-        if (fourOClockAM.compareTo(endTime) < 0) {
+    public void endTime(LocalDateTime endTime) throws Exception {
+        int fourOClockAM = 4;
+        if (fourOClockAM < endTime.getHour() && endTime.getHour() < 17) {
             throw new Exception("Outside of working hours.");
         }
         this.endTime = endTime;
     }
 
-    public double getPay() {
+    public double getPay() throws Exception {
+        if (startTime.isAfter(endTime)) {
+            throw new Exception("Can't end work before you start.");
+        }
         return 15.00;
     }
 }
--- src/test/java/BabysitterPayTest.java
+++ src/test/java/BabysitterPayTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,14 +17,15 @@ public class BabysitterPayTest {
 
     @Test
     public void testABabysitterForFamilyAMakes15DollarsAnHourBefore11PM() throws Exception {
-        babysitterPay.startTime(LocalTime.parse("17:00"));
+        babysitterPay.startTime(LocalDateTime.of(2019, Month.JULY, 12, 17, 0));
+        babysitterPay.endTime(LocalDateTime.of(2019, Month.JULY, 12, 18, 0));
         assertEquals(babysitterPay.getPay(), 15.00);
     }
 
     @Test
     public void testABabySitterDoesNotWorkPriorTo5PM() {
         try {
-            babysitterPay.startTime(LocalTime.parse("14:00"));
+            babysitterPay.startTime(LocalDateTime.of(2019, Month.JULY, 12, 14, 0));
             fail();
         } catch (Exception exception) {
             assertTrue(exception.getMessage().contains("Outside of working hours."));
@@ -33,10 +35,22 @@ public class BabysitterPayTest {
     @Test
     public void testABabySitterDoesNotWorkAfter4AM() {
         try {
-            babysitterPay.endTime(LocalTime.parse("05:00"));
+            babysitterPay.endTime(LocalDateTime.of(2019, Month.JULY, 12, 5, 0));
             fail();
         } catch (Exception exception) {
             assertTrue(exception.getMessage().contains("Outside of working hours."));
+        }
+    }
+
+    @Test
+    public void testABabySitterCannotEndBeforeTheyStart() {
+        try {
+            babysitterPay.startTime(LocalDateTime.of(2019, Month.JULY, 12, 18, 0));
+            babysitterPay.endTime(LocalDateTime.of(2019, Month.JULY, 12, 17, 0));
+            babysitterPay.getPay();
+            fail();
+        } catch (Exception exception) {
+            assertTrue(exception.getMessage().contains("Can't end work before you start."));
         }
     }
 }
