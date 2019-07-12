ff84a4b31ae652e9b0cc054aed7407159af5360f
Nicholas Land <land.d.nicholas@gmail.com>
2019-07-12 13:12:22 -0400

    Refactor Babysitter class
    This class represents the babysitters working hours.

---
+++ src/main/java/Babysitter.java
@@ -0,0 +1,25 @@
+import java.time.LocalDateTime;
+
+public class Babysitter {
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private int fiveOClockPM = 17;
+    private int fourOClockAM = 4;
+
+    public void startTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void endTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    protected void isInWorkingHours() throws Exception {
+        if ((fourOClockAM < endTime.getHour() && endTime.getHour() < fiveOClockPM) ||
+        (fiveOClockPM > startTime.getHour() && startTime.getHour() > fourOClockAM)) {
+            throw new Exception("Outside of working hours.");
+        }
+        if (startTime.isAfter(endTime)) { throw new Exception("Can't end work before you start."); }
+    }
+}
--- src/test/java/BabysitterPayTest.java
+++ src/test/java/BabysitterPayTest.java
@@ -8,27 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BabysitterPayTest {
 
-    private BabysitterPay babysitterPay;
+    private Babysitter babysitter;
 
     @BeforeEach
     public void setUp() {
-        babysitterPay = new BabysitterPay("Family A");
-    }
-
-    @Test
-    public void testABabysitterForFamilyAMakes15DollarsAnHourBefore11PM() throws Exception {
-        LocalDateTime julyTwelfth2019At5PM = LocalDateTime.of(2019, Month.JULY, 12, 17, 0);
-        LocalDateTime julyTwelfth2019At6PM = LocalDateTime.of(2019, Month.JULY, 12, 18, 0);
-        babysitterPay.startTime(julyTwelfth2019At5PM);
-        babysitterPay.endTime(julyTwelfth2019At6PM);
-        assertEquals(babysitterPay.getPay(), 15.00);
+        babysitter = new Babysitter();
     }
 
     @Test
     public void testABabySitterDoesNotWorkPriorTo5PM() {
         try {
             LocalDateTime julyTwelfth2019At2PM = LocalDateTime.of(2019, Month.JULY, 12, 14, 0);
-            babysitterPay.startTime(julyTwelfth2019At2PM);
+            babysitter.startTime(julyTwelfth2019At2PM);
+            babysitter.endTime(julyTwelfth2019At2PM);
+            babysitter.isInWorkingHours();
             fail();
         } catch (Exception exception) {
             assertTrue(exception.getMessage().contains("Outside of working hours."));
@@ -39,7 +32,9 @@ public class BabysitterPayTest {
     public void testABabySitterDoesNotWorkAfter4AM() {
         try {
             LocalDateTime julyTwelfth2019At5AM = LocalDateTime.of(2019, Month.JULY, 12, 5, 0);
-            babysitterPay.endTime(julyTwelfth2019At5AM);
+            babysitter.startTime(julyTwelfth2019At5AM);
+            babysitter.endTime(julyTwelfth2019At5AM);
+            babysitter.isInWorkingHours();
             fail();
         } catch (Exception exception) {
             assertTrue(exception.getMessage().contains("Outside of working hours."));
@@ -51,9 +46,9 @@ public class BabysitterPayTest {
         LocalDateTime julyTwelfth2019At6PM = LocalDateTime.of(2019, Month.JULY, 12, 18, 0);
         LocalDateTime julyTwelfth2019At5Pm = LocalDateTime.of(2019, Month.JULY, 12, 17, 0);
         try {
-            babysitterPay.startTime(julyTwelfth2019At6PM);
-            babysitterPay.endTime(julyTwelfth2019At5Pm);
-            babysitterPay.getPay();
+            babysitter.startTime(julyTwelfth2019At6PM);
+            babysitter.endTime(julyTwelfth2019At5Pm);
+            babysitter.isInWorkingHours();
             fail();
         } catch (Exception exception) {
             assertTrue(exception.getMessage().contains("Can't end work before you start."));
