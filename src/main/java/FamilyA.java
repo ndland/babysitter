dd95e7037de24d16b016f935009e27007ca474b4
Nicholas Land <land.d.nicholas@gmail.com>
2019-07-12 15:53:38 -0400

    Test for full shift with FamilyA is done

--- src/main/java/FamilyA.java
+++ src/main/java/FamilyA.java
@@ -4,17 +4,23 @@ import java.time.temporal.ChronoUnit;
 public class FamilyA extends Babysitter {
 
     private LocalTime elevenPM = LocalTime.of(23, 0);
+    private LocalTime fourAM = LocalTime.of(4, 0);
     private double payBefore11PM = 15.00;
+    private double payAfter11PM = 20.00;
     private double totalPay = 0.0;
 
     public double getPay() throws Exception {
         super.isInWorkingHours();
-        if (getEndTime().toLocalTime().compareTo(LocalTime.MIDNIGHT)<= 0) {
-            totalPay = (24 - ChronoUnit.HOURS.between(getEndTime().toLocalTime(), elevenPM)) * 20.00 +
-                    (24l + ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime())) * payBefore11PM;
+        if (getEndTime().toLocalTime().compareTo(elevenPM) >= 0 || getEndTime().toLocalTime().compareTo(fourAM) <= 0) {
+            totalPay = (24 - ChronoUnit.HOURS.between(getEndTime().toLocalTime(), elevenPM)) * payAfter11PM +
+                    ChronoUnit.HOURS.between(getStartTime().toLocalTime(), elevenPM) * payBefore11PM;
         } else {
-            totalPay = ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime()) * payBefore11PM;
+            totalPay = calculatePayBefore11PM();
         }
         return totalPay;
+    }
+
+    private double calculatePayBefore11PM() {
+        return ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime()) * payBefore11PM;
     }
 }
--- src/test/java/FamilyATest.java
+++ src/test/java/FamilyATest.java
@@ -38,12 +38,26 @@ public class FamilyATest {
     @Test
     public void testABabysitterForFamilyAMakes35DollarsForWorking1HourBefore11PMAnd1HourAfter11PM() {
         LocalDateTime julyTwelfth2019At11PM = LocalDateTime.of(2019, Month.JULY, 12, 23, 0);
-        LocalDateTime julyTwelfth2019At12AM = LocalDateTime.of(2019, Month.JULY, 13, 0, 0);
+        LocalDateTime julyThirteenth2019At12AM = LocalDateTime.of(2019, Month.JULY, 13, 0, 0);
         FamilyA familyA = new FamilyA();
         familyA.startTime(julyTwelfth2019At11PM);
-        familyA.endTime(julyTwelfth2019At12AM);
+        familyA.endTime(julyThirteenth2019At12AM);
         try {
             assertEquals(35, familyA.getPay());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testABabysitterForFamilyAMakes190DollarsForWorkingAFullShiftForFamilyA() {
+        LocalDateTime julyTwelfth2019At5PM = LocalDateTime.of(2019, Month.JULY, 12, 17, 0);
+        LocalDateTime julyThirteenth2019At4AM = LocalDateTime.of(2019, Month.JULY, 13, 4, 0);
+        FamilyA familyA = new FamilyA();
+        familyA.startTime(julyTwelfth2019At5PM);
+        familyA.endTime(julyThirteenth2019At4AM);
+        try {
+            assertEquals(190, familyA.getPay());
         } catch (Exception e) {
             e.printStackTrace();
         }
