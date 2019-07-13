c41c53a046322593a0b1e3c2d0d01ada3848da5f
Nicholas Land <land.d.nicholas@gmail.com>
2019-07-13 11:08:00 -0400

    Add new test for working until Midnight for FamilyB

--- src/main/java/FamilyB.java
+++ src/main/java/FamilyB.java
@@ -8,9 +8,14 @@ public class FamilyB extends Babysitter {
     public double getPay() throws Exception {
         super.isInWorkingHours();
 
+        if (getStartTime().toLocalTime().compareTo(LocalTime.MIDNIGHT) == 0 || getEndTime().toLocalTime().compareTo(LocalTime.MIDNIGHT) == 0) {
+            return (24 + ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime())) * 8.0;
+        }
+
         if (getStartTime().toLocalTime().compareTo(tenPM) >= 0) {
             return ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime()) * 8.0;
         }
+
         return ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime()) * 12.0;
     }
 }
--- src/test/java/FamilyBTest.java
+++ src/test/java/FamilyBTest.java
@@ -43,13 +43,26 @@ public class FamilyBTest {
     }
 
     @Test
-    public void testABabysitterForFamilyBMakes8DollarsForWorking1HourBetween8PMAnd10PM() {
+    public void testABabysitterForFamilyBMakes8DollarsForWorking1HourBetween10PMAnd12AM() {
         LocalDateTime julyTwelfth2019At10PM = LocalDateTime.of(2019, Month.JULY, 12, 22, 0);
         LocalDateTime julyTwelfth2019At11PM = LocalDateTime.of(2019, Month.JULY, 12, 23, 0);
         familyB.startTime(julyTwelfth2019At10PM);
         familyB.endTime(julyTwelfth2019At11PM);
         try {
             assertEquals(8.00, familyB.getPay());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testABabysitterForFamilyBMakes16DollarsForWorking2HourBetween10PMAnd12AM() {
+        LocalDateTime julyTwelfth2019At10PM = LocalDateTime.of(2019, Month.JULY, 12, 22, 0);
+        LocalDateTime julyThirteenth2019At12AM = LocalDateTime.of(2019, Month.JULY, 13, 0, 0);
+        familyB.startTime(julyTwelfth2019At10PM);
+        familyB.endTime(julyThirteenth2019At12AM);
+        try {
+            assertEquals(16.00, familyB.getPay());
         } catch (Exception e) {
             fail();
         }
