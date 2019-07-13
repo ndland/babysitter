768701fd3ef4ebb414ba558c5048fbb2d73f1c70
Nicholas Land <land.d.nicholas@gmail.com>
2019-07-13 11:12:07 -0400

    Refactor FamilyB class

--- src/main/java/FamilyB.java
+++ src/main/java/FamilyB.java
@@ -3,19 +3,21 @@ import java.time.temporal.ChronoUnit;
 
 public class FamilyB extends Babysitter {
 
-    private LocalTime tenPM = LocalTime.of(22, 0);
+    private static final double BASE_PAY_BEFORE_10PM = 12.0;
+    private static final double PAY_BETWEEN_10PM_AND_12AM = 8.0;
+    private static final LocalTime TEN_PM = LocalTime.of(22, 0);
 
     public double getPay() throws Exception {
         super.isInWorkingHours();
 
         if (getStartTime().toLocalTime().compareTo(LocalTime.MIDNIGHT) == 0 || getEndTime().toLocalTime().compareTo(LocalTime.MIDNIGHT) == 0) {
-            return (24 + ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime())) * 8.0;
+            return (24 + ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime())) * PAY_BETWEEN_10PM_AND_12AM;
         }
 
-        if (getStartTime().toLocalTime().compareTo(tenPM) >= 0) {
-            return ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime()) * 8.0;
+        if (getStartTime().toLocalTime().compareTo(TEN_PM) >= 0) {
+            return ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime()) * PAY_BETWEEN_10PM_AND_12AM;
         }
 
-        return ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime()) * 12.0;
+        return ChronoUnit.HOURS.between(getStartTime().toLocalTime(), getEndTime().toLocalTime()) * BASE_PAY_BEFORE_10PM;
     }
 }
